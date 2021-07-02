a01f36ad0687e92547a48554733a0d22f8b50d0b
Ronaldo Carneiro <ronaldo.carneiro@efi.com>
2021-07-02 20:55:18 -0300

    first commit

---
+++ src/DEVIO.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DevIO.Business.Models;
+using DEVIO.Api.ViewModels;
+
+namespace DEVIO.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
